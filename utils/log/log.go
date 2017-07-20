714b6ccb9443f8b139421dcadf5a405bf3451ed5
刘水文 <liushuiwen@yunfan.com>
2017-07-20 15:48:23 +0800

    update log

--- utils/log/log.go
+++ utils/log/log.go
@@ -1,91 +1,40 @@
 package log
 
 import (
-	"net/http"
-	"os"
-
 	"github.com/Sirupsen/logrus"
-	lumberjack "gopkg.in/natefinch/lumberjack.v2"
+	"github.com/lswen/common/utils/golog"
 )
 
-// Constants for logger.
-const (
-	AccessLogFilePath      = "log/access"
-	AccessLogFileExtension = ".txt"
-	AccessLogMaxSize       = 5 // megabytes
-	AccessLogMaxBackups    = 7
-	AccessLogMaxAge        = 30 //days
-	ErrorLogFilePath       = "log/error"
-	ErrorLogFileExtension  = ".json"
-	ErrorLogMaxSize        = 10 // megabytes
-	ErrorLogMaxBackups     = 7
-	ErrorLogMaxAge         = 30 //days
-)
+var log *golog.Logger = golog.New("log")
 
-func LumberJackLogger(filePath string, maxSize int, maxBackups int, maxAge int) *lumberjack.Logger {
-	return &lumberjack.Logger{
-		Filename:   filePath,
-		MaxSize:    maxSize, // megabytes
-		MaxBackups: maxBackups,
-		MaxAge:     maxAge, //days
-	}
-}
-
-func InitLogToStdoutDebug() {
-	logrus.SetFormatter(&logrus.TextFormatter{ForceColors: true, FullTimestamp: true, TimestampFormat: "2006-01-02 15:04:05"})
-	logrus.SetOutput(os.Stdout)
-	logrus.SetLevel(logrus.DebugLevel)
-}
-
-func InitLogToStdout() {
-	logrus.SetFormatter(&logrus.TextFormatter{ForceColors: true, FullTimestamp: true, TimestampFormat: "2006-01-02 15:04:05"})
-	logrus.SetOutput(os.Stdout)
-	logrus.SetLevel(logrus.WarnLevel)
-}
-
-func InitLogToFile() {
-	logrus.SetFormatter(&logrus.JSONFormatter{})
-	out := LumberJackLogger(ErrorLogFilePath+ErrorLogFileExtension, ErrorLogMaxSize, ErrorLogMaxBackups, ErrorLogMaxAge)
-	logrus.SetOutput(out)
-	logrus.SetLevel(logrus.WarnLevel)
-}
-
-// Init logrus
-func Init(environment string) {
-	switch environment {
-	case "DEVELOPMENT":
-		InitLogToStdoutDebug()
-	case "TEST":
-		InitLogToFile()
-	case "PRODUCTION":
-		InitLogToFile()
-	}
-	logrus.Debugf("Environment : %s", environment)
+func init() {
+	golog.SetLevelByString("log", "debug")
+	golog.EnableColorLogger("log", true)
 }
 
 // Debug logs a message at level Debug on the standard logger.
 func Debug(args ...interface{}) {
-	logrus.Debug(args...)
+	log.Debugln(args...)
 }
 
 // Print logs a message at level Info on the standard logger.
 func Print(args ...interface{}) {
-	logrus.Print(args...)
+	log.Infoln(args)
 }
 
 // Info logs a message at level Info on the standard logger.
 func Info(args ...interface{}) {
-	logrus.Info(args...)
+	log.Infoln(args...)
 }
 
 // Warn logs a message at level Warn on the standard logger.
 func Warn(args ...interface{}) {
-	logrus.Warn(args...)
+	log.Warnln(args...)
 }
 
 // Warning logs a message at level Warn on the standard logger.
 func Warning(args ...interface{}) {
-	logrus.Warning(args...)
+	logrus.Warnln(args...)
 }
 
 // Error logs a message at level Error on the standard logger.
@@ -150,49 +99,35 @@ func Debugln(args ...interface{}) {
 
 // Println logs a message at level Info on the standard logger.
 func Println(args ...interface{}) {
-	logrus.Println(args...)
+	log.Infoln(args...)
 }
 
 // Infoln logs a message at level Info on the standard logger.
 func Infoln(args ...interface{}) {
-	logrus.Infoln(args...)
+	log.Infoln(args...)
 }
 
 // Warnln logs a message at level Warn on the standard logger.
 func Warnln(args ...interface{}) {
-	logrus.Warnln(args...)
+	log.Warnln(args...)
 }
 
 // Warningln logs a message at level Warn on the standard logger.
 func Warningln(args ...interface{}) {
-	logrus.Warningln(args...)
+	log.Warnln(args...)
 }
 
 // Errorln logs a message at level Error on the standard logger.
 func Errorln(args ...interface{}) {
-	logrus.Errorln(args...)
+	log.Errorln(args...)
 }
 
 // Panicln logs a message at level Panic on the standard logger.
 func Panicln(args ...interface{}) {
-	logrus.Panicln(args...)
+	log.Errorln(args...)
 }
 
 // Fatalln logs a message at level Fatal on the standard logger.
 func Fatalln(args ...interface{}) {
-	logrus.Fatalln(args...)
-}
-
-// log response body data for debugging
-func DebugResponse(response *http.Response) string {
-	bodyBuffer := make([]byte, 5000)
-	var str string
-	count, err := response.Body.Read(bodyBuffer)
-	for ; count > 0; count, err = response.Body.Read(bodyBuffer) {
-		if err != nil {
-		}
-		str += string(bodyBuffer[:count])
-	}
-	Debugf("response data : %v", str)
-	return str
+	log.Fatalln(args...)
 }
